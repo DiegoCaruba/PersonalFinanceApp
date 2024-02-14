be2d4337f43200f08a10cf9a3a6779c903da279e
Diego Caruba <diego.caruba@gmail.com>
2024-02-13 22:11:31 -0300

    feat: add navbar components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CategoryListComponent } from './pages/categories/category-list/category-list.component';
+import { CategoryFormComponent } from './pages/categories/category-form/category-form.component';
+import { CategoriesRoutingModule } from './pages/categories/categories-routing.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CategoriesRoutingModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'personalFinanceApp';
 }

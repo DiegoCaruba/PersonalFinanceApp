ff7b836eaa2b2e6937c0c35372ce3a4c319d27b0
Diego Caruba <diego.caruba@gmail.com>
2024-03-20 22:34:22 -0300

    add categy model

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    RouterModule
   ]
 })
 export class CategoriesModule { }
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css'
 })
